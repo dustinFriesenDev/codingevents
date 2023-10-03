6ad5e11c58061db03bf8823b8d81a099ab405e29
Dustin Dev <94726570+dustinFriesenDev@users.noreply.github.com>
2023-10-03 14:35:28 -0400

    Through create a Model and about to walkthrough Create a Data Layer chapter 14

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,38 +1,37 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
-    private static HashMap<String, String> events = new HashMap<>();
+    private static List<Event> events = new ArrayList<>();
 
 
     @GetMapping
     public String displayAllEvents(Model model){
-        events.put("Menteaship","A fun meetup for connecting with mentors");
-        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
-        events.put("Javascripty", "An imaginary meetup for Javascript developers");
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
     //lives at /events/create (events is from requestMapping up top)
     @GetMapping("create")
-    public String processCreateEventForm(){
+    public String displayCreateEventForm(Model model){
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     //lives at /events/create (they handle different methods and can live at the same path.)
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription){
-        events.put(eventName, eventDescription);
+    public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription){
+        events.add(new Event(eventName, eventDescription));
         return "redirect:/events";
     }
 
