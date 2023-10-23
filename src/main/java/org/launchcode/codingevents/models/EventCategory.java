703181cb568d7da83e2d8514d0f9f23c87845c6e
Dustin Dev <94726570+dustinFriesenDev@users.noreply.github.com>
2023-10-23 13:58:55 -0400

    exercises, studios, and lessons all pushed at one time.

---
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -0,0 +1,43 @@
+package org.launchcode.codingevents.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class EventCategory extends AbstractEntity{
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+    private String name;
+
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
+
+    public EventCategory(int id, String name) {
+        this.name = name;
+    }
+
+    public EventCategory(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
